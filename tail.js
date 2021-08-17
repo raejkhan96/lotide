58e4dab56187895587c9d340c90f5cd399c414b1
Rae J. Khan <raekhan18@gmail.com>
2021-08-17 00:51:09 +0000

    update

---
+++ tail.js
@@ -0,0 +1,20 @@
+const tail = function(words) {
+  let arr = [];
+  let length = words.length;
+  arr = words.slice(1, length);
+  return arr;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected)  {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
