01d5d29d24bed87f077cbfd15e3c3ea5676ff437
Rae J. Khan <raekhan18@gmail.com>
2021-08-17 00:16:52 +0000

    Update

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected)  {
-    console.log('✅ ✅ ✅ Assertion Passed: %s === %s', actual, expected);
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log('🛑🛑🛑 Assertion Failed: %s !== %s', actual, expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
