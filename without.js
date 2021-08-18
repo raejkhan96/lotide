5324e79e0f4e2e242607d3e69826e10db4d5a4c2
Rae J. Khan <raekhan18@gmail.com>
2021-08-18 01:35:02 +0000

    update

---
+++ without.js
@@ -0,0 +1,34 @@
+const without = (arr1, arr2) => {
+  let arr3 = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (!arr2.includes(arr1[i])) {
+      arr3.push(arr1[i]);
+    }
+  }
+  console.log(arr3);
+};
+
+const assertArrayEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true)  {
+    console.log(`✅ ✅ ✅ Assertion Passed`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]);
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+// Make sure the original array was not altered by the without function
+assertArrayEqual(words, ["hello", "world", "lighthouse"]);
