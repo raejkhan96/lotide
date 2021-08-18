3f6796367460cbe24aadb0c764dd99a603529fad
Rae J. Khan <raekhan18@gmail.com>
2021-08-18 22:12:11 +0000

    Updated

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const findKeyByValue = function(shows, value) {
+  for (let key in shows) {
+    if (shows[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected)  {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+// console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
